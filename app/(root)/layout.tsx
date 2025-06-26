6afca4d8383931ab0cd2c8b73ccb22d67321cef6
Dasun Dushmantha <dasunathauda99@gmail.com>
2025-06-26 14:46:12 +0530

    implement git auth and storage

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -5,24 +5,22 @@ import { redirect } from "next/navigation";
 
 import { isAuthenticated } from "@/lib/actions/auth.action";
 
-const RootLayout = async ({ children }: { children: ReactNode }) => {
+const Layout = async ({ children }: { children: ReactNode }) => {
     const isUserAuthenticated = await isAuthenticated();
-
-    if (!isUserAuthenticated) redirect('/');
+    if (!isUserAuthenticated) redirect("/sign-in");
 
     return (
         <div className="root-layout">
             <nav>
                 <Link href="/" className="flex items-center gap-2">
-                    <Image src="/logo.svg" alt="logo"
-                           width={38}
-                           height={32} />
+                    <Image src="/logo.svg" alt="MockMate Logo" width={38} height={32} />
                     <h2 className="text-primary-100">PrepWise</h2>
                 </Link>
             </nav>
 
             {children}
         </div>
-    )
-}
-export default RootLayout
+    );
+};
+
+export default Layout;
